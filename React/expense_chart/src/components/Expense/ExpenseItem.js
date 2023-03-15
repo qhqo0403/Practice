10796dd37b48b236be7146886134b132eff44de4
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-03-15 22:43:00 +0900

    basicReact

--- React/expense_chart/src/components/Expense/ExpenseItem.js
+++ React/expense_chart/src/components/Expense/ExpenseItem.js
@@ -4,6 +4,9 @@ import Card from '../UI/Card';
 
 
 function ExpenseItem(props) {
+  const clickHandler = () => {
+    console.log('clicked!!')
+  }
 
   return (
     <Card className="expense-item">
@@ -12,6 +15,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Click here!</button>
     </Card>
   )
 }
