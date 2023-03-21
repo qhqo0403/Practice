53b20302e026b6aa3ce1d1d2bbc91efb5c425b51
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-03-21 14:15:20 +0900

    filterComponent

---
+++ React/expense_chart/src/components/Expense/ExpenseFilter.js
@@ -0,0 +1,24 @@
+import './ExpenseFilter.css'
+
+const ExpenseFilter = (props) => {
+
+  const enteredYearHandler = (event) => {
+    props.onChangeFilter(event.target.value);
+  }
+
+  return(
+    <div className='expenses-filter'>
+      <div className='expenses-filter__control'>
+        <label>Filter by year</label>
+        <select value={props.selected} onChange={enteredYearHandler}>
+          <option>2022</option>
+          <option>2021</option>
+          <option>2020</option>
+          <option>2019</option>
+        </select>
+      </div>
+    </div>
+  )
+}
+
+export default ExpenseFilter;
