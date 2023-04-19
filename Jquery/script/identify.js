935331f6381145e93bf5f3b0a18d32f24bb2c0de
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-04-20 00:44:21 +0900

    javascript version

--- Jquery/script/identify.js
+++ Jquery/script/identify.js
@@ -1,4 +1,4 @@
-const email = $('#email');
+/* const email = $('#email');
 const password = $('#password');
 const submitBtn = $('.submit-btn');
 
@@ -13,5 +13,46 @@ submitBtn.click(function() {
     setTimeout(function() {
       password.next('span').removeClass('warning');
     }, 1500);
+  }
+}); */
+
+const emailInput = document.getElementById('email');
+const pwInput = document.getElementById('password');
+const submitBtn = document.querySelector('.submit-btn');
+const labels = document.querySelectorAll('.input-field span');
+
+let enteredEmail = emailInput.value;
+let enteredPw = pwInput.value;
+
+const validInput = (input) => {
+  const div = input.closest('div');
+  const label = div.querySelector('span');
+  label.classList.add('warning');
+  setTimeout(() => {
+    label.classList.remove('warning');
+  }, 1000);
+}
+
+const onChangeHandler = (event) => {
+  const input = event.target;
+  const div = input.closest('div');
+  const label = div.querySelector('span');
+
+  if (event.target.value) {
+    label.classList.add('valid');
+  } else {
+    label.classList.remove('valid');
+  }
+}
+
+emailInput.addEventListener('change', onChangeHandler);
+pwInput.addEventListener('change', onChangeHandler);
+
+submitBtn.addEventListener('click', (event) => {
+  if (enteredEmail.trim().length === 0) {
+    validInput(emailInput);
+    return;
+  } else if (enteredPw.trim().length === 0) {
+    validInput(pwInput);
   }
 });
