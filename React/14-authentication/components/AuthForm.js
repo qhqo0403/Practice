25f47478f0d0a5751a55ed87cc66ffab3cf24d4f
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-05-09 16:12:46 +0900

    router and authentication

---
+++ React/14-authentication/components/AuthForm.js
@@ -0,0 +1,41 @@
+import { Form, Link, useSearchParams } from 'react-router-dom';
+
+import classes from './AuthForm.module.css';
+
+function AuthForm() {
+  // 쿼리 매개변수에 접근하는 훅 = useSearchParams. 사용하는 모습은 state와 비슷해서 배열분해구조로 현재의 쿼리 매개변수를 얻는 객체와 업데이트 하는 함수로 이루어져 있음!(여기서는 함수를 사용하지 않기때문에 할당하지 않음)
+  const [searchParams] = useSearchParams();
+  // 매개변수 객체에서 값을 가져오는 메서드 get
+  const isLogin = searchParams.get('mode') === 'login';
+
+  return (
+    <>
+      <Form method="post" className={classes.form}>
+        <h1>{isLogin ? 'Log in' : 'Create a new user'}</h1>
+        <p>
+          <label htmlFor="email">Email</label>
+          <input id="email" type="email" name="email" required />
+        </p>
+        <p>
+          <label htmlFor="image">Password</label>
+          <input id="password" type="password" name="password" required />
+        </p>
+        <div className={classes.actions}>
+          <Link to={`?mode=${isLogin ? 'signup' : 'login'}`}>
+            {isLogin ? 'Create new user' : 'Login'}
+          </Link>
+          <button>Save</button>
+        </div>
+      </Form>
+    </>
+  );
+}
+
+export default AuthForm;
+/* 
+  const [isLogin, setIsLogin] = useState(true);
+
+  function switchAuthHandler() {
+    setIsLogin((isCurrentlyLogin) => !isCurrentlyLogin);
+  }
+ */
---
+++ React/14-authentication/pages/Authentication.js
@@ -0,0 +1,42 @@
+import { json, redirect } from 'react-router-dom';
+import AuthForm from '../components/AuthForm';
+
+function AuthenticationPage() {
+  return <AuthForm />;
+}
+
+export default AuthenticationPage;
+
+export const action = async ({request}) => {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get('mode') || 'login';
+  console.log(searchParams);
+  // 잘못된 url을 입력했을 때의 오류검증
+  if (mode !== 'login' && mode !== 'signup') {
+    throw json({message: 'Unsupported mode'}, {status: 422});
+  }
+
+  const data = await request.formData();
+  const authData = {
+    email: data.get('email'),
+    password : data.get('password')
+  }
+  console.log(data);
+  const response = await fetch('http://localhost:8080/' + mode, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(authData),
+  });
+  // 입력오류 혹은 유효하지 않은 자격증명으로 로그인하려는 경우 백엔드에서 설정한 오류 출력
+  if (response.status === 422 || response.status === 401) {
+    return response;
+  }
+
+  if (!response.ok) {
+    throw json({message: 'Could not authentication user'}, {status: 500});
+  }
+
+  return redirect('/');
+}
