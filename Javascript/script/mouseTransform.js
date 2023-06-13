6b8b78b5aa606bba3c8af16c9ffa731907b4279f
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-06-13 14:39:46 +0900

    mouse transform

---
+++ Javascript/script/mouseTransform.js
@@ -0,0 +1,31 @@
+const cursor = document.querySelector('.cursor');
+const circle = document.querySelector('.circle');
+const hoverBtns = document.querySelectorAll('.bottom a');
+
+let x = 0;
+let y = 0;
+let targetX = 0;
+let targetY = 0;
+
+hoverBtns.forEach(btn => {
+  btn.addEventListener('mouseenter', () => {
+    circle.style.transform = "scale(0.2)";
+    circle.style.backgroundColor = "rosybrown";
+  });
+  btn.addEventListener('mouseleave', () => {
+    circle.style.transform = "scale(1)";
+    circle.style.backgroundColor = null;
+  })
+})
+
+
+window.addEventListener('mousemove', e => {
+  x = e.pageX;
+  y = e.pageY;
+})
+
+const reaction = () => {
+  cursor.style.transform = `translate(${x}px, ${y}px)`;
+  window.requestAnimationFrame(reaction);
+}
+reaction();
