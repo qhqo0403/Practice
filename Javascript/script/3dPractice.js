53867df922a1eb3d2f01ef6fe8b069238c0dbd05
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-07-04 17:07:47 +0900

    javascript css transform

--- Javascript/script/3dPractice.js
+++ Javascript/script/3dPractice.js
@@ -1,12 +1,18 @@
 (() => {
   const space = document.querySelector('.space');
   const progressBar = document.querySelector('.progress-bar');
+  const stage = document.querySelector('.stage');
+
+  const mousePos = {
+    x: 0,
+    y: 0
+  };
 
   let maxScrollNum = 0;
 
   const resizeHandler = () => {
     maxScrollNum = document.body.scrollHeight - window.innerHeight;
-  }
+  };
 
   window.addEventListener('scroll', () => {
     let percent = scrollY / maxScrollNum;
@@ -14,6 +20,13 @@
 
     space.style.transform = `translateZ(${zValue - 490}vw)`;
     progressBar.style.width = `${percent * 100}%`;
+  });
+
+  window.addEventListener('mousemove', e => {
+    mousePos.x = e.pageX - (window.innerWidth / 2);
+    mousePos.y = e.pageY - (window.innerHeight / 2);
+
+    stage.style.transform = `rotateX(${mousePos.y / 130}deg) rotateY(${mousePos.x / 130}deg)`;
   });
 
   window.addEventListener('resize', resizeHandler);
