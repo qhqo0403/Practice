5e0987292782adf8f809c38d1581e3b371f2ded1
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-07-06 02:47:18 +0900

    end

--- Javascript/script/3dPractice.js
+++ Javascript/script/3dPractice.js
@@ -2,6 +2,7 @@
   const space = document.querySelector('.space');
   const progressBar = document.querySelector('.progress-bar');
   const stage = document.querySelector('.stage');
+  const selectChar = document.querySelector('.select-character');
 
   const mousePos = {
     x: 0,
@@ -29,39 +30,112 @@
 
   /* character class */
   class Character {
-    constructor(position) {
+    constructor(data) {
       this.charEl = document.createElement('div');
       this.charEl.classList.add('character');
-      this.charEl.innerHTML = `이미지 추가`;
+      this.charEl.innerHTML = `이미지추가`;
 
       stage.appendChild(this.charEl);
-
-      this.charEl.style.left = `${position.x}%`
+      this.xPos = data.x;
+      this.charEl.style.left = `${this.xPos}%`
       this.scrollState = false;
+      this.currentScroll = 0;
+      this.speed = data.speed;
+      this.direction;
+      this.walking = false;
+      this.requestId;
       this.init();
+      this.keyEvent();
     };
   };
-  
+  // 스크롤 관련 메서드
   Character.prototype.init = function() {
     window.addEventListener('scroll', () => {
+
       clearTimeout(this.scrollState);
 
       if (!this.scrollState) {
         this.charEl.classList.add('running');
       };
-      
+
       this.scrollState = setTimeout(() => {
         this.scrollState = false;
         this.charEl.classList.remove('running');
       }, 500);
+
+      if (this.currentScroll > scrollY) {
+        this.charEl.dataset.direction = 'backward';
+      } else {
+        this.charEl.dataset.direction = 'forward';
+      }
+      // 바로 이전값과 현재값을 비교하려면 if문 이후에 대입해줘야함!
+      this.currentScroll = scrollY;
+
     });
   };
+  // 키보드 이벤트 관련 메서드
+  Character.prototype.keyEvent = function() {
+    window.addEventListener('keydown', (e) => {
+      if (this.walking) {
+        return;
+      }
+
+      if (e.keyCode === 37) {
+        this.direction = 'left';
+        this.charEl.dataset.direction = 'left';
+        this.charEl.classList.add('running');
+        this.move();
+        this.walking = true;
+      } else if (e.keyCode === 39) {
+        this.direction = 'right';
+        this.charEl.dataset.direction = 'right';
+        this.charEl.classList.add('running');
+        this.move();
+        this.walking = true;
+      };
+    });
+
+    window.addEventListener('keyup', () => {
+      this.charEl.classList.remove('running');
+      this.walking = false;
+      cancelAnimationFrame(this.requestId);
+    });
+  };
+  // 부드러운 움직임
+  Character.prototype.move = function() {
+    if (this.direction === 'left') {
+      this.xPos -= this.speed;
+    } else {
+      this.xPos += this.speed;
+    }
+
+    if (this.xPos < 2) {
+      this.xPos = 2;
+    }
+    if (this.xPos > 88) {
+      this.xPos = 88;
+    }
+
+    this.charEl.style.left = `${this.xPos}%`
+    
+    this.requestId = requestAnimationFrame(this.move.bind(this));
+  };
+
+
 
   stage.addEventListener('click', e => {
     new Character({
-      x: (e.clientX / window.innerWidth) * 100
+      x: (e.clientX / window.innerWidth) * 100,
+      speed: (Math.random() * 0.5) + 0.2
     });
   });
+
+  selectChar.addEventListener('click', e => {
+    const mode = e.target.dataset.char;
+    document.body.dataset.char = mode;
+  });
+
+
 
   const resizeHandler = () => {
     maxScrollNum = document.body.scrollHeight - window.innerHeight;
