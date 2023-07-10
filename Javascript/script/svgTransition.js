8cf08fb3bdf0fb9a1617a6613e0f388cca54244c
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-07-10 23:18:22 +0900

    javascript control svg

--- Javascript/script/svgTransition.js
+++ Javascript/script/svgTransition.js
@@ -1,11 +1,14 @@
 // svg에 자바스크립트 코드를 적용하려면 img 태그가 아니라 object 태그를 사용해야함!
 
 const svg = document.querySelector('.key');
-const svgDoc = svg.contentDocument; // svg 문서 내부를 가져오는 방법
+// svg 문서는 비동기식으로 가져오기 때문에 load 이벤트가 꼭 필요함
+svg.addEventListener('load', () => {
+  const svgDoc = svg.contentDocument; // svg 문서를 가져오는 방법
 
-const keyBtn = svgDoc.querySelector('.key-btn');
-const keyBlade = svgDoc.querySelector('.key-blade');
+  const keyBtn = svgDoc.querySelector('.key-btn');
+  const keyBlade = svgDoc.querySelector('.key-blade');
 
-keyBtn.addEventListener('click', () => {
-  keyBlade.classList.toggle('key-blade-filpped');
+  keyBtn.addEventListener('click', () => {
+    keyBlade.classList.toggle('key-blade-flipped');
+  });
 });
