e3fb162aaf645423373c35e06c1492f4195a4757
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-08-06 22:12:54 +0900

    practice pattern

--- algorithm/section05.js
+++ algorithm/section05.js
@@ -28,5 +28,156 @@ function sameFrequency(num1, num2){
   return true;
 };
 
+// 강의 솔루션
+function sameFrequency01(num1, num2){
+  let strNum1 = num1.toString();
+  let strNum2 = num2.toString();
+  if(strNum1.length !== strNum2.length) return false;
+  
+  let countNum1 = {};
+  let countNum2 = {};
+  
+  for(let i = 0; i < strNum1.length; i++){
+    countNum1[strNum1[i]] = (countNum1[strNum1[i]] || 0) + 1
+  }
+  
+  for(let j = 0; j < strNum1.length; j++){
+    countNum2[strNum2[j]] = (countNum2[strNum2[j]] || 0) + 1
+  }
+  
+  for(let key in countNum1){
+    if(countNum1[key] !== countNum2[key]) return false;
+  }
 
-// Frequency Counter / Multiple Pointers - areThereDuplicates
+  return true;
+}
+
+
+// Frequency Counter / Multiple Pointers - areThereDuplicates : 인자들 사이에 중복된 숫자가 있으면 true 아니면 false;
+// 빈도패턴으로 해결함
+function areThereDuplicates() {
+  // 인자가 없다면 false 
+  const args = [...arguments]
+  if (args.length === 0) {
+      return false;
+  }
+  const numbers = {};
+  // 인자들을 객체에 담음. 중복되는 요소가 있다면 1씩 증가
+  for (let num of args) {
+      numbers[args[num]] = (numbers[args[num]] || 0) + 1; 
+  }
+  // 1 이상의 값이 있는지 확인해서 중복된 숫자가 있는지 확인
+  for (let key in numbers) {
+    if (numbers[key] > 1) {
+        return true;
+    }  
+  }
+  return false;
+};
+
+// 강의 솔루션
+function areThereDuplicates01(...args) {
+  // Two pointers
+  args.sort((a,b) => a - b);
+  let start = 0;
+  let next = 1;
+  while(next < args.length){
+    if(args[start] === args[next]){
+        return true
+    }
+    start++
+    next++
+  }
+  return false
+}
+
+function areThereDuplicates02() {
+  return new Set(arguments).size !== arguments.length;
+}
+
+
+// Multiple Pointers - averagePair : 정렬된 배열과 숫자를 인수로 받고 배열의 특정 두 요소의 합을 2로 나눈 값이 num과 일치하는지 확인
+function averagePair(arr, num){
+  // add whatever parameters you deem necessary - good luck!
+  // 빈 배열일 경우 false
+  if (arr.length === 0) {
+      return false;
+  };
+  // 정렬된 배열이니까 양 끝에서 포인터 출발
+  let min = 0;
+  let max = arr.length - 1;
+  // 두 개의 요소를 더해서 2로 나눈 값이 num과 같으면 true
+  while (min < max) {
+      let result = (arr[min] + arr[max]) / 2;
+      if (result === num) {
+          return true;
+    // 두 개의 요소를 더해서 2로 나눈 값이 num보다 크면 오른쪽에서 -1, 작으면 왼쪽에서 +1
+      } else if (result > num) {
+          max--;
+      } else {
+          min++;
+      }
+  }
+  return false;
+};
+
+
+// Multiple Pointers - isSubsequence : 두 개의 문자열을 인자로 받고 첫 번째 문자열의 글자가 두 번째 문자열에 포함되는지 확인
+function isSubsequence(first, second) {
+  // 첫 번째 문자열이 두 번째 문자열보다 길 경우
+  if (first.length > second.length) {
+      return false;
+  };
+  // 첫 번째 문자열의 첫 글자와 두 번째 문자열의 첫 글자를 비교.
+  // 같은 문자면 첫번째 문자열 인덱스 +1, 다르면 두 번째 문자열 인덱스 + 1
+  let i = 0;
+  for (let j = 0; j < second.length; j++) {
+    if (first[i] === second[j]) {
+        i++;
+    }
+    if (i === (first.length -1)) {
+        return true;
+    }
+  }
+  return false;
+}
+
+// 강의 솔루션
+function isSubsequence(str1, str2) {
+  var i = 0;
+  var j = 0;
+  if (!str1) return true;
+  while (j < str2.length) {
+    if (str2[j] === str1[i]) i++;
+    if (i === str1.length) return true;
+    j++;
+  }
+  return false;
+}
+
+
+// Sliding Window - maxSubarraySum : 정수배열과 숫자 n을 인자로 받는 함수에서 배열의 요소를 n개 더했을 때 가장 큰 값을 반환. 배열의 n개는 연속적으로 이루어져야함
+function maxSubarraySum(arr, num){
+  // add whatever parameters you deem necessary - good luck!
+  // 배열의 길이가 num 보다 작은 경우
+  if (arr.length < num) {
+      return null;
+  }
+  let result = 0;
+  let tempSum = 0;
+  // num만큼의 첫번째 합
+  for (let i = 0; i < num; i++) {
+      result += arr[i];
+  }
+  
+  tempSum = result;
+  for (let j = num; j < arr.length; j++) {
+      // tempSum 에서 num에 해당하는 배열의 첫번째 요소를 빼주고 num번째의 숫자를 더해줌
+      tempSum = tempSum - arr[j - num] + arr[j];
+      result = Math.max(tempSum, result);
+  }
+  return result;
+}
+
+
+// 남은 두 문제는 나중에 다시보기!
