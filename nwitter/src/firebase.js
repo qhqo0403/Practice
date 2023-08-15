100aadde4b308f27a150e0ebab35ca648aff66dc
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-08-15 16:51:06 +0900

    clone coding initial setting

---
+++ nwitter/src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCEbesvhPvQZc73Gtek3m1FDrkZfnZOnX8",
+  authDomain: "nwitter-d3a16.firebaseapp.com",
+  projectId: "nwitter-d3a16",
+  storageBucket: "nwitter-d3a16.appspot.com",
+  messagingSenderId: "428435541511",
+  appId: "1:428435541511:web:990759b87fe8c10381e4a2"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
