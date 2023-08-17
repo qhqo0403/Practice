830567c4b12580b786f58dd4b5008a16b4d62d98
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-08-17 21:44:05 +0900

    create env file

--- nwitter/src/firebase.js
+++ nwitter/src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCEbesvhPvQZc73Gtek3m1FDrkZfnZOnX8",
-  authDomain: "nwitter-d3a16.firebaseapp.com",
-  projectId: "nwitter-d3a16",
-  storageBucket: "nwitter-d3a16.appspot.com",
-  messagingSenderId: "428435541511",
-  appId: "1:428435541511:web:990759b87fe8c10381e4a2"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
