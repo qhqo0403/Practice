c5accc582c01b3baf5ef362fdd4e0f84b7009e23
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-08-25 21:37:51 +0900

    binary search

--- algorithm/section09.js
+++ algorithm/section09.js
@@ -2,7 +2,7 @@
 
 // 선형 검색(linear search) : 특정 값을 찾을 때 한 번의 하나의 항목을 확인하면서 모든 항목을 탐색
 // 자바스크립트 메서드로는 indexOf, includes, find, findIndex
-// 데이터가 정렬되어있지 않거나 규칙이 없을 때
+// 데이터가 분류되어있지 않을 때
 // O(n)으로 배열의 길이가 길면 길수록 오래걸림
 
 // 연습문제 : 배열과 숫자를 인수로 받고 숫자가 배열안에 포함되어 있는지 확인하는 함수
@@ -13,4 +13,28 @@ function linearSearch(arr, n){
       }
   }
   return -1;
+}
+
+
+// 이진 검색(binary search) : 중간 지점에서부터 시작해서 항목을  찾을 때까지 범위를 좁혀감
+// 분할과 정복
+// 데이터가 분류되어 있거나 순서가 있어야함
+// 평균적으로는 O(log n) 이고 최선의 경우에는 O(1)
+
+// 연습문제 : 정렬된 배열과 숫자를 인수로 받고 해당 숫자가 배열안에 있는지, 있다면 인덱스를 반환하는 함수
+function binarySearch(arr, n){
+  // add whatever parameters you deem necessary - good luck!
+  let left = 0;
+  let right = arr.length - 1;
+  let mid = Math.floor((left + right) / 2); // 시작점과 끝점의 중간점(평균) 계산하려면 
+  
+  while (left < right && arr[mid] !== n) {
+      if (arr[mid] < n) {
+          left = mid + 1;
+      } else {
+          right = mid - 1;
+      }
+      mid = Math.floor((left + right) / 2);
+  }
+  return arr[mid] === n ? mid : -1;
 }
