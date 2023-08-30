d6a9e9a6d55379e3e38cbd3c5f12e8b76ffbfec2
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-08-30 21:42:01 +0900

    merge sort

--- algorithm/section14.js
+++ algorithm/section14.js
@@ -28,3 +28,20 @@ function merge(arr1, arr2) {
   }
   return result;
 }
+
+// 배열을 작은 단위로 분할하고 다시 병합 -> 배열을 작은단위로 분할하기 위해 재귀방식 사용
+function mergeSort(arr) {
+  if (arr.length === 1) { // 배열 요소가 1개가 되면 재귀함수를 종료하고 1개의 요소가 담긴 배열을 반환
+    return arr;
+  }
+  let mid = Math.floor(arr.length / 2); // 중간 지점 구하기
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+
+  return merge(left, right);
+}
+
+// 합병 정렬의 빅오표기 : 시간복잡도는 평균적, 최적, 최악 상황에서 모두 O(n log n), 공간복잡도는 O(n)
+// 시간 복잡도는 배열의 길이가 길어질수록 log n 의 배수만큼 분할하게 되고, 병합하기위해 요소를 비교하기 위해서는 O(n)이 소요되기 때문에 최종적으로 O(n log n)
+// 예를 들어 배열의 길이가 8이면 3번의 분할, 배열의 길이가 32라면 5번의 분할
+// 공간 복잡도는 배열을 분할하여 새로운 배열을 만들기 때문에 그만큼 메모리를 차지하게 됨
