5b2ebe756ac2872e3a87f72895017c054531d1de
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-09-06 23:34:10 +0900

    singly linked list - push, pop, shift, unshift

--- algorithm/section18.js
+++ algorithm/section18.js
@@ -9,3 +9,73 @@
 // 배열 vs 연결리스트
 // 연결리스트에는 인덱스가 없기 때문에 요소로 임의 접근이 불가능함 -> 맨 처음부터 하나하나 살펴봐야함
 // 새로운 요소를 처음이나 중간에 삽입하려고 하는 경우 배열은 인덱스의 재배치가 필요한 반면 연결리스트는 그럴 필요가 없음
+
+class Node {
+  constructor(val) { // 어떤 데이터던지 받아서 저장하는 생성자 함수
+    this.val = val;
+    this.next = null; // 다음 노드를 가리키는 포인터
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) { // 새로운 노드를 마지막에 추가하는 메서드
+    let newNode = new Node(val);
+    if (!this.head) { // 저장된 노드가 없다면(헤드가 없다면) 새로 저장되는 노드가 헤드이자 테일이 되도록
+      this.head = newNode;
+      this.tail = this.head;
+    } else { // 헤드가 있다면 그 헤드의 다음 노드 포인터를 새로 추가되는 노드를 가리키고, 새로운 노드를 테일로 만들어줌
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++; // 전체 리스트의 길이
+    return this;
+  }
+
+  pop() { // 마지막 노드를 삭제하는 메서드. 양방향 연결 리스트가 아니기 때문에 끝에서부터 접근할 수 없고 헤드부터 거슬러올라가야함
+    if (!this.head) return undefined; // 노드가 하나도 없을 때 undefined 출력
+    let current = this.head; // 헤드노드부터 시작
+    let newTail = current; // 새로운 테일이 될 노드를 저장하는 변수
+    while (current.next) { // 마지막 요소에 도달하게 되면 반복문 종료 -> 마지막 요소는 next가 null
+      newTail = current; // newTail 은 항상 current 를 따라가게 됨
+      current = current.next;
+    } // tail에 도달하면 next가 없기 때문에 반복문이 종료되고 마지막 요소 하나 전(뒤에서 두번째 노드)이 newTail에 저장된 상태
+    this.tail = newTail; // tail을 새로 지정해주고
+    this.tail.next = null; // 이전에 있던 tail과의 연결을 지워주기
+    this.length--; // 연결리스트 길이 감소
+    if (this.length === 0) { // 노드가 다 삭제되었다면 -> 이 로직이 없으면 길이는 감소해도 head와 tail 이 같은 노드를 가리키는 상태로 남아있게 됨
+      this.head = null;
+      this.tail = null;
+    }
+    return current; // 지우려는 요소를 반환
+  }
+
+  shift() { // 첫 번째 요소 제거하는 메서드
+    if (!this.head) return undefined;
+    let currentHead = this.head; // 현재의 헤드노드를 저장
+    this.head = currentHead.next; // 현재의 헤드노드의 다음 노드를 리스트의 헤드로 변경
+    this.length--; // 길이를 감소시키고
+    if (this.length === 0) { // 길이가 0 이 되었을 때 tail을 null 로 -> head 는 길이가 줄어들면서 next를 통해 null 이 됨
+      this.tail = null;
+    }
+    return currentHead;
+  }
+
+  unshift(val) { // 노드를 맨 앞에 추가하는 메서드
+    let newNode = new Node(val); // 새로운 노드 생성
+    if (!this.head) { // 헤드노드가 없다면 헤드와 테일이 같은 노드를 가리키게 됨
+      this.head = newNode;
+      this.tail = this.head;
+    } else { // 헤드노드가 이미 있다면 새로운 노드의 포인터를 현재 헤드로 만들어주고 리스트의 헤드를 업데이트 -> else로 감싸지 않으면 새로운 헤드노드의 next가 무한대로 생성됨
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++; // 길이 증가
+    return this;
+  }
+}
