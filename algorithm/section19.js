141b7f103f56105203a2c1df2995099fa403cf0c
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-09-09 02:27:43 +0900

    doubly linked list - push, pop, shift, unshift

--- algorithm/section19.js
+++ algorithm/section19.js
@@ -15,4 +15,62 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   };
+
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) { // this.length === 0 으로 해도 됨! 헤드가 없거나 리스트의 길이가 0일 때
+      this.head = newNode;
+      this.tail = this.head;
+    } else { // 이미 노드가 있을 때
+      this.tail.next = newNode; // 테일의 넥스트 포인터를 연결하고
+      newNode.prev = this.tail; // 새로운 노드의 prev 를 테일과 연결
+      this.tail = newNode; // 테일에 새로운 노드를 할당
+    }
+    this.length++;
+    return this;
+  };
+
+  pop() { // 단일연결리스트와 다르게 prev 와 next 모두 끊어줘야함
+    if (!this.head) return undefined;
+    let poped = this.tail; // 제거될 노드를 변수로 저장
+    if (this.length === 1) { // 마지막 노드일 경우
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poped.prev;
+      this.tail.next = null;
+      poped.prev = null;
+    };
+    this.length--;
+    return poped;
+  };
+
+  shift() { // 맨 앞에서 노드를 삭제하는 것은 포인터를 하나 더 삭제한다는 것 빼고는 단방향 리스트와 완전 같음!
+    if (!this.head) return undefined;
+    let shifted = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = shifted.next;
+      this.head.prev = null;
+      shifted.next = null;
+    };
+    this.length--;
+    return shifted;
+  };
+
+  unshift(val) { // shift와 마찬가지로 단방향 리스트와 거의 유사
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode
+    }
+    this.length++;
+    return this;
+  }
 };
