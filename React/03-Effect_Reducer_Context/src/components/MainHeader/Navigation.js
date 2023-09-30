04284e2277fe50b534fa77ec372b9359017e823a
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-09-30 23:41:08 +0900

    remind - initialize

--- React/03-Effect_Reducer_Context/src/components/MainHeader/Navigation.js
+++ React/03-Effect_Reducer_Context/src/components/MainHeader/Navigation.js
@@ -1,32 +1,29 @@
-import React, { useContext } from 'react';
+import React from 'react';
 
 import classes from './Navigation.module.css';
-import AuthContext from '../../store/auth-context';
 
 const Navigation = (props) => {
-  const ctx = useContext(AuthContext);
-
   return (
     <nav className={classes.nav}>
       <ul>
-        {ctx.isLoggedIn && (
+        {props.isLoggedIn && (
           <li>
             <a href="/">Users</a>
           </li>
         )}
-        {ctx.isLoggedIn && (
+        {props.isLoggedIn && (
           <li>
             <a href="/">Admin</a>
           </li>
         )}
-        {ctx.isLoggedIn && (
+        {props.isLoggedIn && (
           <li>
-            <button onClick={ctx.onLogout}>Logout</button>
+            <button onClick={props.onLogout}>Logout</button>
           </li>
         )}
       </ul>
     </nav>
-  )
+  );
 };
 
 export default Navigation;
